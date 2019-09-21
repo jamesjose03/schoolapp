f948f5ae2a6ef3e0ad04bdcdd3c0d0d6c35d084a
jamesjose03 <jamesjose03@gmail.com>
2019-09-21 11:15:04 +0530

    Added Login Page & Validation

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,70 +1,112 @@
 import 'package:flutter/material.dart';
-
-String _email = '';
-String _password = '';
+import 'package:validate/validate.dart';
+import './studentpage.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _LoginPageState();
-  }
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginData {
+  String admnno = '';
+  String password = '';
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  _LoginData _data = new _LoginData();
+
+  String _validateAdNo(String value) {
+    try {
+      Validate.isAlphaNumeric(value);
+    } catch (e) {
+      return 'The Admission Number must be a valid one!';
+    }
+
+    return null;
+  }
+
+  String _validatePassword(String value) {
+    if (value.length < 8) {
+      return 'The Password must be at least 8 characters.';
+    }
+
+    return null;
+  }
+
+  void submit() {
+    // First validate form.
+    if (this._formKey.currentState.validate()) {
+      _formKey.currentState.save(); // Save our form now.
+
+      print('Printing the login data.');
+      print('Admission Number: ${_data.admnno}');
+      print('Password: ${_data.password}');
+      if('${_data.admnno}' == '9817' && '${_data.password}' == 'aaabbbcccddd')
+      {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => StudentPage()));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     appBar: AppBar(
-       title: Text('Sign In'),
-     ),
-     body: Column(
-      children: <Widget> [ 
-      Padding(
-    padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
-    child: new TextFormField(
-      maxLines: 1,
-      keyboardType: TextInputType.emailAddress,
-      autofocus: false,
-      decoration: new InputDecoration(
-          hintText: 'Admission Number',
-          icon: new Icon(
-            Icons.mail,
-            color: Colors.grey,
-          )),
-      validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
-      onSaved: (value) => _email = value.trim(),
-    ),
-  ), 
-  Padding(
-    padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
-    child: new TextFormField(
-      maxLines: 1,
-      obscureText: true,
-      autofocus: false,
-      decoration: new InputDecoration(
-          hintText: 'Password',
-          icon: new Icon(
-            Icons.lock,
-            color: Colors.grey,
-          )),
-      validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
-      onSaved: (value) => _password = value.trim(),
-    ),
-  ),
-  new Padding(
-      padding: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
-      child: new MaterialButton(
-        onPressed: () {},
-        elevation: 5.0,
-        minWidth: 200.0,
-        height: 42.0,
-        color: Colors.blue,
-        child: Text('Login', style: new TextStyle(fontSize: 20.0, color: Colors.white)
-      )
-  )
-  )
-      ]
-     ) 
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return new Scaffold(
+      backgroundColor: Color(0xFFFFE6B4),
+      appBar: new AppBar(
+        title: new Text('Login'),
+        backgroundColor: Color(0xFFB38867),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(20.0),
+        child: new Form(
+          key: this._formKey,
+          child: new ListView(
+            children: <Widget>[
+              new TextFormField(
+                keyboardType: TextInputType.number, 
+                decoration: new InputDecoration(
+                  hintText: 'Eg: 9817',
+                  labelText: 'Admission Number'
+                ),
+                validator: this._validateAdNo,
+                onSaved: (String value) {
+                  this._data.admnno = value;
+                }
+              ),
+              new TextFormField(
+                obscureText: true, // Use secure text for passwords.
+                decoration: new InputDecoration(
+                  hintText: 'Password',
+                  labelText: 'Enter your password'
+                ),
+                validator: this._validatePassword,
+                onSaved: (String value) {
+                  this._data.password = value;
+                }
+              ),
+              new Container(
+                width: screenSize.width,
+                child: new RaisedButton(
+                  child: new Text(
+                    'Login',
+                    style: new TextStyle(
+                      color: Colors.white
+                    ),
+                  ),
+                  onPressed: this.submit,
+                  color: Color(0xFF626D71),
+                ),
+                margin: new EdgeInsets.only(
+                  top: 20.0
+                ),
+              )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
